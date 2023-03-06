e215771c0f6df88607f83af9ba9dfee05b7f3a7b
asucanyh-cn <asucanyh@outlook.com>
2023-03-06 18:57:29 +0800

    update

--- src/db/MyDb.java
+++ src/db/MyDb.java
@@ -4,9 +4,10 @@ public class MyDb {
 	private static Connection conn=null;
 	public static Connection getConn() throws Exception{
 		Class.forName("com.mysql.jdbc.Driver");
-		String url="jdbc:mysql://localhost:3306/sinograindb?characterEncoding=utf-8";
+//		String url="jdbc:mysql://localhost:3306/sinograindb?characterEncoding=utf-8";
+		String url="jdbc:mysql://192.168.31.5:3306/sinograindb?characterEncoding=utf-8";
 		String user="root";
-		String password="";
+		String password="8326179";
 		conn=DriverManager.getConnection(url, user, password);
 		return conn;
 	}
